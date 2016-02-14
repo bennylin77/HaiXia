15a871c7ceb52e002fd3e97a216e7d58b7b47675
bennylin77 <bennylin77@gmail.com>
2016-02-15 01:09:12 +0800

    first commit

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -1,8 +1,8 @@
 class Experience < ActiveRecord::Base
   has_attached_file :image, 
-                    styles: {}
+                    styles: { original: "1024"}
                      
-  validates_attachment :image, 
+  validates_attachment :image,
                        content_type: { content_type: /\Aimage\/.*\Z/, message: "圖片格式錯誤" }, 
                        size: { less_than: 10.megabytes, message: "圖片大小超過10MB" }      
 end
